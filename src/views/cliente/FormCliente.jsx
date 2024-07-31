97b42f3ef5923c8dea72460de2e8c0aa9f5d846c
Geovane Júnior <geovanejosedasilvajr@gmail.com>
2024-07-31 10:47:59 -0300

    adicionando endereço

--- src/views/cliente/FormCliente.jsx
+++ src/views/cliente/FormCliente.jsx
@@ -1,42 +1,45 @@
-import axios from "axios"
-import React, { useState, useEffect } from "react"
-import InputMask from "react-input-mask"
-import { Link, useLocation } from "react-router-dom"
-import { Button, Container, Divider, Form, Icon } from "semantic-ui-react"
-import MenuSistema from "../../MenuSistema"
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import InputMask from "react-input-mask";
+import { Button, Container, Divider, Form, FormGroup, Icon } from "semantic-ui-react";
+import MenuSistema from "../../MenuSistema";
+import { Link, useLocation } from "react-router-dom";
 
 export default function FormCliente() {
-  const { state } = useLocation()
-  const [idCliente, setIdCliente] = useState()
+  const { state } = useLocation();
+  const [idCliente, setIdCliente] = useState();
 
-  const [nome, setNome] = useState()
-  const [cpf, setCpf] = useState()
-  const [dataNascimento, setDataNascimento] = useState()
-  const [foneCelular, setFoneCelular] = useState()
-  const [foneFixo, setFoneFixo] = useState()
+  const [endereco, setEndereco] = useState([]);
+  const [nome, setNome] = useState();
+  const [cpf, setCpf] = useState();
+  const [dataNascimento, setDataNascimento] = useState();
+  const [foneCelular, setFoneCelular] = useState();
+  const [foneFixo, setFoneFixo] = useState();
+  const [primeiroClique, setPrimeiroClique] = useState(true);
 
   useEffect(() => {
     if (state != null && state.id != null) {
       axios
         .get("http://localhost:8080/api/cliente/" + state.id)
         .then((response) => {
-          setIdCliente(response.data.id)
-          setNome(response.data.nome)
-          setCpf(response.data.cpf)
-          setDataNascimento(formatarData(response.data.dataNascimento))
-          setFoneCelular(response.data.foneCelular)
-          setFoneFixo(response.data.foneFixo)
-        })
+          setIdCliente(response.data.id);
+          setNome(response.data.nome);
+          setCpf(response.data.cpf);
+          setDataNascimento(formatarData(response.data.dataNascimento));
+          setFoneCelular(response.data.foneCelular);
+          setFoneFixo(response.data.foneFixo);
+          setEndereco(response.data.enderecos || []);
+        });
     }
-  }, [state])
+  }, [state]);
 
   function formatarData(dataParam) {
     if (dataParam === null || dataParam === "" || dataParam === undefined) {
-      return ""
+      return "";
     }
 
-    let arrayData = dataParam.split("-")
-    return arrayData[2] + "/" + arrayData[1] + "/" + arrayData[0]
+    let arrayData = dataParam.split("-");
+    return arrayData[2] + "/" + arrayData[1] + "/" + arrayData[0];
   }
 
   function salvar() {
@@ -46,29 +49,77 @@ export default function FormCliente() {
       dataNascimento: dataNascimento,
       foneCelular: foneCelular,
       foneFixo: foneFixo,
-    }
+    };
 
     if (idCliente != null) {
       //Alteração:
       axios
         .put("http://localhost:8080/api/cliente/" + idCliente, clienteRequest)
         .then((response) => {
-          console.log("Cliente alterado com sucesso.")
+          console.log("Cliente alterado com sucesso.");
+          resetForm();
         })
         .catch((error) => {
-          console.log("Erro ao alter um cliente.")
-        })
+          console.log("Erro ao alterar um cliente.");
+        });
     } else {
       //Cadastro:
       axios
         .post("http://localhost:8080/api/cliente", clienteRequest)
         .then((response) => {
-          console.log("Cliente cadastrado com sucesso.")
+          let clienteID = response.data.id;
+          enviarEnderecos(clienteID);
+          console.log("Cliente cadastrado com sucesso.");
+          resetForm();
         })
         .catch((error) => {
-          console.log("Erro ao incluir o cliente.")
-        })
+          console.log("Erro ao incluir o cliente.");
+        });
     }
+  }
+  function resetForm() {
+    setIdCliente(undefined);
+    setNome('');
+    setCpf('');
+    setDataNascimento('');
+    setFoneCelular('');
+    setFoneFixo('');
+    setEndereco([]);
+    setPrimeiroClique(true);
+  }
+
+  function enviarEnderecos(clienteID) {
+    endereco.forEach(async (endereco) => {
+      await axios
+        .post(
+          "http://localhost:8080/api/cliente/endereco/" + clienteID,
+          endereco
+        )
+        .then(() => console.log("Endereço cadastrado com sucesso."))
+        .catch((error) => console.log("Erro ao cadastrar endereço.", error));
+    });
+  }
+
+  function adicionarEndereco() {
+    setEndereco([
+      ...endereco,
+      {
+        rua: "",
+        numero: "",
+        cep: "",
+        bairro: "",
+        cidade: "",
+        estado: "",
+        complemento: "",
+      },
+    ]);
+    setPrimeiroClique(false);
+  }
+
+  function atualizarEndereco(index, campo, valor) {
+    const novosEnderecos = [...endereco];
+    novosEnderecos[index][campo] = valor;
+    setEndereco(novosEnderecos);
   }
 
   return (
@@ -76,58 +127,48 @@ export default function FormCliente() {
       <MenuSistema tela={"cliente"} />
 
       <div style={{ marginTop: "3%" }}>
-        <Container textAlign='justified'>
+        <Container textAlign="justified">
           {idCliente === undefined && (
             <h2>
               {" "}
               <span style={{ color: "darkgray" }}>
                 {" "}
                 Cliente &nbsp;
-                <Icon
-                  name='angle double right'
-                  size='small'
-                />{" "}
+                <Icon name="angle double right" size="small" />{" "}
               </span>{" "}
               Cadastro
             </h2>
           )}
+
           {idCliente !== undefined && (
             <h2>
               {" "}
               <span style={{ color: "darkgray" }}>
                 {" "}
                 Cliente &nbsp;
-                <Icon
-                  name='angle double right'
-                  size='small'
-                />{" "}
+                <Icon name="angle double right" size="small" />{" "}
               </span>{" "}
               Alteração
             </h2>
           )}
-
           <Divider />
 
           <div style={{ marginTop: "4%" }}>
             <Form>
-              <Form.Group widths='equal'>
+              <Form.Group widths="equal">
                 <Form.Input
                   required
                   fluid
-                  label='Nome'
-                  maxLength='100'
+                  label="Nome"
+                  maxLength="100"
                   value={nome}
                   onChange={(e) => setNome(e.target.value)}
                 />
 
-                <Form.Input
-                  required
-                  fluid
-                  label='CPF'
-                >
+                <Form.Input required fluid label="CPF">
                   <InputMask
                     required
-                    mask='999.999.999-99'
+                    mask="999.999.999-99"
                     value={cpf}
                     onChange={(e) => setCpf(e.target.value)}
                   />
@@ -135,42 +176,27 @@ export default function FormCliente() {
               </Form.Group>
 
               <Form.Group>
-                <Form.Input
-                  required
-                  fluid
-                  label='Fone Celular'
-                  width={6}
-                >
+                <Form.Input fluid label="Fone Celular" width={6}>
                   <InputMask
-                    mask='(99) 99999.9999'
+                    mask="(99) 9999.9999"
                     value={foneCelular}
                     onChange={(e) => setFoneCelular(e.target.value)}
                   />
                 </Form.Input>
 
-                <Form.Input
-                  required
-                  fluid
-                  label='Fone Fixo'
-                  width={6}
-                >
+                <Form.Input fluid label="Fone Fixo" width={6}>
                   <InputMask
-                    mask='(99) 9999.9999'
+                    mask="(99) 9999.9999"
                     value={foneFixo}
                     onChange={(e) => setFoneFixo(e.target.value)}
                   />
                 </Form.Input>
 
-                <Form.Input
-                  required
-                  fluid
-                  label='Data Nascimento'
-                  width={6}
-                >
+                <Form.Input fluid label="Data Nascimento" width={6}>
                   <InputMask
-                    mask='99/99/9999'
+                    mask="99/99/9999"
                     maskChar={null}
-                    placeholder='Ex: 20/03/1985'
+                    placeholder="Ex: 20/03/1985"
                     value={dataNascimento}
                     onChange={(e) => setDataNascimento(e.target.value)}
                   />
@@ -178,38 +204,131 @@ export default function FormCliente() {
               </Form.Group>
             </Form>
 
-            <div style={{ marginTop: "4%" }}>
-
-                <Link to={'/list-cliente'} >
-              <Button
-                type='button'
+            {endereco.map((address, index) => (
+              <div key={index} style={{ marginBottom: "2%" }}>
+                <Divider />
+                <Form>
+                  <FormGroup>
+                    <Form.Input
+                      required
+                      fluid
+                      label="Rua"
+                      width={8}
+                      value={address.rua}
+                      onChange={(e) =>
+                        atualizarEndereco(index, "rua", e.target.value)
+                      }
+                    />
+                    <Form.Input
+                      required
+                      fluid
+                      label="Numero"
+                      width={6}
+                      value={address.numero}
+                      onChange={(e) =>
+                        atualizarEndereco(index, "numero", e.target.value)
+                      }
+                    />
+                    <Form.Input fluid label="CEP" width={6}>
+                      <InputMask
+                        mask="99999-999"
+                        value={address.cep}
+                        onChange={(e) =>
+                          atualizarEndereco(index, "cep", e.target.value)
+                        }
+                      />
+                    </Form.Input>
+                  </FormGroup>
+                  <FormGroup>
+                    <Form.Input
+                      required
+                      fluid
+                      label="Bairro"
+                      width={6}
+                      value={address.bairro}
+                      onChange={(e) =>
+                        atualizarEndereco(index, "bairro", e.target.value)
+                      }
+                    />
+                    <Form.Input
+                      required
+                      fluid
+                      label="Cidade"
+                      width={6}
+                      value={address.cidade}
+                      onChange={(e) =>
+                        atualizarEndereco(index, "cidade", e.target.value)
+                      }
+                    />
+                    <Form.Input
+                      required
+                      fluid
+                      label="Estado"
+                      width={6}
+                      value={address.estado}
+                      onChange={(e) =>
+                        atualizarEndereco(index, "estado", e.target.value)
+                      }
+                    />
+                  </FormGroup>
+                  <Form.Input
+                    required
+                    fluid
+                    label="Complemento"
+                    value={address.complemento}
+                    onChange={(e) =>
+                      atualizarEndereco(index, "complemento", e.target.value)
+                    }
+                  />
+                </Form>
+              </div>
+            ))}
+            <Button
                 inverted
                 circular
                 icon
-                labelPosition='left'
-                color='orange'
-              >
-                <Icon name='reply' />
-                Voltar
-              </Button>
-              </Link>
-
-              <Button
-                inverted
-                circular
-                icon
-                labelPosition='left'
-                color='blue'
-                floated='right'
+                labelPosition="left"
+                color="blue"
+                floated="right"
                 onClick={() => salvar()}
               >
-                <Icon name='save' />
+                <Icon name="save" />
                 Salvar
-              </Button>
+            </Button>
+
+            <Button
+              type="button"
+              inverted
+              circular
+              icon
+              labelPosition="left"
+              color="green"
+              floated="right"
+              onClick={adicionarEndereco}
+            >
+              <Icon name="plus" />
+              {primeiroClique ? "Adicionar Endereço" : "Adicionar um Novo Endereço"}
+            </Button>
+
+            <div style={{ marginTop: "6%" }}>
+              <Link to={"/list-cliente"}>
+                <Button
+                  type="button"
+                  inverted
+                  circular
+                  icon
+                  labelPosition="left"
+                  color="orange"
+                >
+                  <Icon name="reply" />
+                  <Link to={"/list-cliente"}>Voltar</Link>
+                </Button>
+              </Link>
+
             </div>
           </div>
         </Container>
       </div>
     </div>
-  )
+  );
 }
